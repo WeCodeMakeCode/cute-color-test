87ab1c47ca533494620da8f5b96a9e323d29a812
WeCodeMakeCode <52891037+WeCodeMakeCode@users.noreply.github.com>
2020-10-20 18:26:54 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,34 @@
-let mySpriteColor = 0
-let myRandomColor = cuteColor.randomColor(2, 3, 4)
-console.logValue("myRandomColor", myRandomColor)
-console.log(cuteColor.colorNameFromNumber(cuteColor.colorIndexPicker(2)))
-console.log(cuteColor.colorNumberFromName("red"))
-let myRandomColorsList = cuteColor.randomColorsList(0, 1, 2)
-let mySpriteList = cuteColor.colorsSpriteList(myRandomColorsList, 16, 16)
-console.logValue("len", mySpriteList.length)
-for (let value of mySpriteList) {
-    value.setPosition(randint(20, 140), randint(20, 100))
-    mySpriteColor = cuteColor.spriteColor(value)
-    console.logValue("color of sprite", mySpriteColor)
-    console.log(cuteColor.colorNameFromNumber(mySpriteColor))
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    myColorNumbersList = cuteColor.colorList(0, 0, 0)
+    mySpriteList = cuteColor.colorsSpriteList(myColorNumbersList, 20, 20)
+    messageSprite.say("List length " + mySpriteList.length)
+    pause(2000)
+    outMySpriteList()
+    pause(2000)
+    messageSprite.say("randomized list")
+    cuteColor.randomize(myColorNumbersList)
+    mySpriteList = cuteColor.colorsSpriteList(myColorNumbersList, 20, 20)
+    outMySpriteList()
+})
+function outMySpriteList () {
+    for (let value of mySpriteList) {
+        mySpriteColor = cuteColor.spriteColor(value)
+        value.z = 1
+        value.say("" + mySpriteColor + "  " + cuteColor.colorNameFromNumber(mySpriteColor))
+        pause(2000)
+    }
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    myRandomColor = cuteColor.randomColor(cuteColor.colorIndexPicker(15), 0, 0)
+    messageSprite.say(cuteColor.colorNameFromNumber(myRandomColor))
+    mySpriteList = cuteColor.colorsSpriteList([myRandomColor], 40, 40)
+})
+let myRandomColor = 0
+let mySpriteColor = 0
+let mySpriteList: Sprite[] = []
+let myColorNumbersList: number[] = []
+let messageSprite: Sprite = null
+messageSprite = sprites.create(img`
+    . 
+    `, SpriteKind.Player)
+messageSprite.setPosition(80, 100)
